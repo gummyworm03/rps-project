aa745d3af7fcd6f67fb900f7d2397fb796b1f770
gummyworm03 <blanguage.language37@gmail.com>
2022-09-05 15:32:17 -0700

    Set up main UI

--- script.js
+++ script.js
@@ -1,74 +1,64 @@
-/*Get computer choice - random
-    Create array of computer choices
-    Randomly select item from array
-Get player choice - input
-    prompt
-    standardize case sensitivity
-Compare computer and player choices
-    if player choice > computer choice
-        you win!
-    if computer choice > player choice
-        you lose!
-    if computer choice = player choice
-        tie! 
-        start new round*/
-
+const winner = document.querySelector('.winner');
+const pScore = document.querySelector(".playerScore");
+const cScore = document.querySelector(".computerScore");
+const ties = document.querySelector('.ties');
+const pChoice = document.querySelector(".playerChoice");
+const cChoice = document.querySelector(".computerChoice");
+const resetButton = document.querySelector(".reset");
+const choices = ['rock', 'paper', 'scissors'];
 
 
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
-}
-const choices = ['rock', 'paper', 'scissors'];
+};
+
+let playerScore = 0;
+let computerScore = 0;
+let numTies = 0;
+
+
+const pImgs = document.querySelectorAll('.pImg');
+
+pImgs.forEach((pImg) => {
+    pImg.addEventListener('click', () => {
+        computerSelection = getComputerChoice();
+        playRound(pImg.id, computerSelection);
+        updateWinner();
+        console.log('1', computerSelection, '2', pImg.id)
+    });
+});
+
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection !== computerSelection && computerSelection === choices[1] && playerSelection === choices[0]) {
         computerScore++;
-        return "You Lose! Paper beats Rock" //computer chooses paper, player chooses rock
+        winner.innerText = "The computer won the round...";
     } else if (playerSelection !== computerSelection && computerSelection === choices[1] && playerSelection === choices[2]) {
         playerScore++;
-        return "You Win! Scissors beats Paper" //computer chooses paper, player chooses scissors
+        winner.innerText = "You won the round!";
     } else if (playerSelection !== computerSelection && computerSelection === choices[2] && playerSelection === choices[0]) {
         playerScore++;
-        return "You Win! Rock beats Scissors" //computer chooses scissors, player chooses rock
+        winner.innerText = "You won the round!";
     } else if (playerSelection !== computerSelection && computerSelection === choices[2] && playerSelection === choices[1]) {
         computerScore++;
-        return "You Lose! Rock beats Scissors" //computer chooses scissors, player chooses paper
+        winner.innerText = "The computer won the round...";
     } else if (playerSelection !== computerSelection && computerSelection === choices[0] && playerSelection === choices[1]) {
         playerScore++;
-        return "You Win! Paper beats Rock" //computer chooses rock, player chooses paper
+        winner.innerText = "You won the round!";
     } else if (playerSelection !== computerSelection && computerSelection === choices[0] && playerSelection === choices[2]) {
         computerScore++;
-        return "You Lose! Rock beats Scissors" //computer chooses rock, player scissors
+        winner.innerText = "The computer won the round...";
     } else {
-        return "It's a tie! Play another round...";
+        numTies++;
+        winner.innerText = "It's a tie!";
     }
-}
+};
 
-let playerScore = 0;
-let computerScore = 0;
 
-//console.log(playRound(playerSelection, computerSelection))
-
-/*function scoreBoard() {
-    return score = [playerScore, computerScore];
-}
-let score = 0;*/
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Let's play!").toLowerCase();
-        let computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));  
-    } 
-   if (playerScore > computerScore) {
-        return "You win the game!"
-        } else if (computerScore > playerScore) {
-        return "You lose the game :("
-        } else {
-        return "You tied! Play again."
-        }
-   
-}
-console.log(game())
+function updateWinner() {
+    pScore.innerText = `Score: ${playerScore}`;
+    cScore.innerText = `Score: ${computerScore}`;
+    ties.innerText = `Ties: ${numTies}`;
+};
 
 
